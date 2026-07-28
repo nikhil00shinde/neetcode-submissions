90b7c3833cb4a6c8beaa34c6e6b83e665d4928ac
Nikhil Shinde <82992442+nikhil00shinde@users.noreply.github.com>
2026-07-28 13:45:12 -0500

    Add: lowest-common-ancestor-of-a-binary-tree-iii - submission-1

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree-iii/submission-1.java
@@ -0,0 +1,25 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+        Set<Node> st = new HashSet<>();
+        while(p != null){
+            st.add(p);
+            p = p.parent;
+        }
+
+        while(q != null){
+            if(st.contains(q)) return q;
+            q = q.parent;
+        }
+        return null;
+    }
+}
